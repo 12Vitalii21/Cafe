148c5da281c10f9edd0173e38cd0b65cc0bcc83f
Vitaliy <Vitalik14954@ya.ru>
2022-10-13 00:31:16 +0300

    Cafe nachalo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $('.slaiders').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    horizontal: true,
+    horizontalSwiping: true,
+    touchThreshold: true
+  })
+
+})
